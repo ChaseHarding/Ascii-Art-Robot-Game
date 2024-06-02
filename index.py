a60987a9e7f634b8c10cb9a5d3116c3ebdf70299
ChaseHarding <chaseharding965@gmail.com>
2024-06-02 13:58:23 -0700

    added more choices and while loops for re-attempts

--- index.py
+++ index.py
@@ -47,9 +47,34 @@ while True:
     choice = input("Do you choose the left door or the right door? (left/right): ").lower()
     if choice == 'left':
         print("You enter the left door and find a room full of advanced robotic prototypes.")
-        break
+        print("One of the robots suddenly comes to life and greets you. It asks you for help to complete it's programming.")
+        print("Do you agree to help the robot or do you leave the room? (help/leave)")
+    
+        while True:
+            action = input("What do you do? (help/leave): ").lower()
+            if action == 'help':
+                print("You agree to help the robot. Together, you manage to complete it's programming and it offers to assist you in your quest.")
+                print("The robot guides you to a hidden passage behind the room, leading you to the main control center of the lab")
+                break
+            
+            elif action == 'leave':
+                print("You decide to leave the room, but as you turn around, the door locks.")
+                print("The robot approaches you and in a static voice says 'If you wont help me, You'll join me here forever.")
+                print("Game Over.")
+              
+        
+
     elif choice == 'right':
+        print('''
+              ________________
+              | /~~~~~~~~\ ||||
+              ||          |...|
+              ||          |   |
+              | \________/  O |
+              ~~~~~~~~~~~~~~~
+              ''')
         print("You enter the right door and find a control room with many monitors and buttons")
+        
         break
     else:
         print("Invalid choice. Please type 'left' or 'right'.")
